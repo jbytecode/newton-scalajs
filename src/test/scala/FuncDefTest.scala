f29759bd084a80261fdc03fc4fde76d1d58fadf9
jbytecode <mhsatman@gmail.com>
2026-03-06 08:12:32 +0300

    format source code

--- src/test/scala/FuncDefTest.scala
+++ src/test/scala/FuncDefTest.scala
@@ -6,13 +6,13 @@ class FunctionDefinitions extends munit.FunSuite {
 
   val eps: Double = 1e-6
 
-  test("Quadratic function") {
+  test("Quadratic function"):
     val x = Array(math.Pi, math.exp(1))
     val obtained = Quadratic(x)
     val expected = 0.0
     assertEquals(obtained, expected)
-  }
-  test("Cross Terms function") {
+  
+  test("Cross Terms function"):
     val x = Array(math.Pi, math.exp(1))
     val obtained = CrossTerms(x)
     val expected = 4.269867111336783
@@ -20,8 +20,8 @@ class FunctionDefinitions extends munit.FunSuite {
       math.abs(obtained - expected) < eps,
       s"Expected $expected but got $obtained"
     )
-  }
-  test("Ackley function") {
+  
+  test("Ackley function"): 
     val x = Array(0.0, 0.0)
     val obtained = Ackley(x)
     val expected = 0.0
@@ -29,5 +29,4 @@ class FunctionDefinitions extends munit.FunSuite {
       math.abs(obtained - expected) < eps,
       s"Expected $expected but got $obtained"
     )
-  }
 }
